7b328595380b784a644ea4026c5980a9a8186f78
Hohlov Kirill <36017007+Seraphimsyv@users.noreply.github.com>
2022-09-28 18:01:13 +0300

    Clean file

--- bootstrap.php
+++ bootstrap.php
@@ -7,28 +7,3 @@
             require strtolower($path) . '.php';
         }
     );
-
-    /**
-     * 
-     */
-
-    $sql = new \No_SQL\Classes\SQL_Manager("test.json");
-    $sql->create_collection("test_collection");
-    #$sql->collections["test_collection"]->create_document(["name" => "Kirill"]);
-
-    $test = $sql->collections("test_collection")->getOne();
-    var_dump($test);
-    #var_dump($test);
-    #$test->pass = "pass";
-    #var_dump($test);
-
-    #$result = $sql->collections["test_collection"]->getOne([["name", "=", "Kirill"]]);
-    #$result->set("login", "log");
-    #$result->set("password", "pass");
-    #var_dump($result);
-    #var_dump((array) $result);
-    #$sql->collections["test_collection"]->create_document(["name" => "еуые"]);
-    #var_dump($sql->collections);
-    #print_r($sql->collections);
-    #var_dump($sql->collections);
-    $sql->save();
